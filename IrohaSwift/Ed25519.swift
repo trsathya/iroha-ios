1621dab4b618fc24680de774ee94934afb8829ef
Sathya <trsathya@gmail.com>
2019-04-13 21:40:50 +0530

    Swift 5 support.

--- IrohaSwift/Ed25519.swift
+++ IrohaSwift/Ed25519.swift
@@ -42,7 +42,7 @@ public func sign(publicKey:String,privateKey:String, message:String) -> String{
     var sig: Array<UInt8> = Array(repeating: 0, count: 64)
     var decPubArr = base64toArr(base64str: publicKey, count: 32)
     var decPriArr = base64toArr(base64str: privateKey, count: 64)
-    ed25519_sign(&sig, message, message.characters.count, &decPubArr, &decPriArr)
+    ed25519_sign(&sig, message, message.count, &decPubArr, &decPriArr)
     let encSig = base64_encode(sig, UInt32(sig.count))
     let base64Sig = String(validatingUTF8:UnsafePointer<CChar>(encSig!))!
 
@@ -52,7 +52,7 @@ public func sign(publicKey:String,privateKey:String, message:String) -> String{
 public func verify(publicKey:String, signature:String, message:String) -> Bool{
     let decPubArr = base64toArr(base64str: publicKey, count: 32)
     let decSigArr = base64toArr(base64str: signature, count: 64)
-    let valid = Int(ed25519_verify(decSigArr, message, message.characters.count, decPubArr))
+    let valid = Int(ed25519_verify(decSigArr, message, message.count, decPubArr))
     if valid == 1 {
         return true
     }else{
@@ -64,7 +64,7 @@ func base64toArr(base64str:String, count:Int) -> Array<UInt8>{
     let base64Ptr = UnsafeMutablePointer<Int8>(mutating: base64str)
     let decBase64 = base64_decode(base64Ptr)
     let decArr = Array<UInt8>(UnsafeBufferPointer(start: decBase64, count: count))
-    base64Ptr.deinitialize()
+    base64Ptr.deinitialize(count: count)
 
     return decArr
 }
